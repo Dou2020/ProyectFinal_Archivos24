5a6fb858645618768170f73cde726a1cac8a1fcb
Dou24 <doueduar2018@gmail.com>
2024-10-21 11:59:25 -0600

    add Frontend

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component'
+import {FlowbiteService} from './flowbite.service';
+import { LoginComponent } from "./pages/login/login.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+}) 
+export class AppComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  title = 'Frontend';
+
+  ngOnInit(): void {
+      this.flowbiteService.loadFlowbite(flowbite => {
+        console.log('Flowbite loaded', flowbite)
+      })
+  }
+}
